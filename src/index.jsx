0753c204e32bff579fd05edb4f621009ffab5b67
Mahmoud Sadollahi <mahmoudsadollahi@gmail.com>
2026-06-25 10:54:37 +0100

    Set up Tailwind CSS in the project

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './assets/css/tailwind.css';
 import React from 'react'; 
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
